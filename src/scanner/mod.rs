411bb2688c4ee33ff5a22575b32d94e8ac49a2e6
sourtaste000 <me@sourtaste000.dev>
2021-12-25 20:24:48 -0800

    Error Handling

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -11,10 +11,31 @@ pub fn scan_to_file() {
     };
     for item in directory {
         if item.is_ok() {
-            let item = item.unwrap();
-            if item.file_name().to_str().unwrap().ends_with(".jar") {
-                let data = serde_json::from_str::<fabric_json::Root>(extract_resources(item.path().to_str().unwrap()).as_str()).unwrap();
+            let item = match item {
+                Ok(item) => item,
+                Err(_) => continue
+            };
+            let item_name = item.file_name();
+            let item_name_str = match item_name.to_str() {
+                Some(item_name) => item_name,
+                None => continue
+            };
+            if item_name_str.ends_with(".jar") {
+                let item_path = item.path();
+                let item_path_str = match item_path.to_str() {
+                    Some(item_path) => item_path,
+                    None => continue
+                };
+                let extracted_resources = match extract_resources(item_path_str) {
+                    Some(extracted_resources) => extracted_resources,
+                    None => continue
+                };
+                let data = match serde_json::from_str::<fabric_json::Root>(extracted_resources.as_str()) {
+                    Ok(data) => data,
+                    Err(_) => continue
+                };
                 if fetch_not_404(format!("https://api.modrinth.com/api/v1/mod/{}", data.id).as_str()) {
+                    println!("Added {}.", data.id);
                     config.fabric.mods.push(data.id);
                 }
             }
@@ -22,7 +43,7 @@ pub fn scan_to_file() {
             continue;
         }
     }
-    let mut scanned_config = fs::File::create("scanned_config.toml").expect("Failed to create file");
+    let mut scanned_config = fs::File::create("scanned_config.toml").expect("Failed to create file.");
     scanned_config.write_all(toml::to_string(&config).unwrap().as_bytes()).unwrap();
 }
 
@@ -35,14 +56,25 @@ fn fetch_not_404(url: &str) -> bool {
     false
 }
 
-fn extract_resources(file: &str) -> String {
+fn extract_resources(file: &str) -> Option<String> {
     let path = std::path::Path::new(&file);
-    let file = std::fs::File::open(&path).unwrap();
-    let mut archive = zip::ZipArchive::new(&file).unwrap();
+    let file = match std::fs::File::open(&path) {
+        Ok(file) => file,
+        Err(_) => return None
+    };
+    let mut archive = match zip::ZipArchive::new(&file) {
+        Ok(archive) => archive,
+        Err(_) => return None
+    };
 
-    let mut resources = archive.by_name("fabric.mod.json").unwrap();
+    let mut resources = match archive.by_name("fabric.mod.json") {
+        Ok(resources) => resources,
+        Err(_) => return None
+    };
 
     let mut contents = String::new();
-    resources.read_to_string(&mut contents).unwrap();
-    contents
+    return match resources.read_to_string(&mut contents) {
+        Ok(_) => Some(contents),
+        Err(_) => None
+    }
 }
